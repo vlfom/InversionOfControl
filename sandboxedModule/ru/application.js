adccd0a83a29c8157c675da1588c63f556b90b7f
Volodymyr Fomenko <vlfomenk@gmail.com>
2016-03-10 10:25:16 +0200

    Add global key inspector

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -2,6 +2,10 @@
 // прикладного приложения, загружаемого в песочницу демонстрационным
 // кусочком фреймворка. Читайте README.md в нем задания.
 
+for (var k in global) {
+	console.log('<Key: ' + k + '> <Type: ' + typeof k + '>');
+}
+
 module.exports = {
 	testFunction: function(a, b, c, d) {
 		return 42;
